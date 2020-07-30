ee39af0949758ba699317225cec05f0baf6b111f
karankurbur <karankurbur@gmail.com>
2020-07-29 18:35:38 -0700

    Nodemon

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser);
-const port = 3000;
+app.use(bodyParser.json());
+const port = 3001;
+
+const a = 4;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/test", (req, res) => {
+  res.send({ friend: 1 });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
