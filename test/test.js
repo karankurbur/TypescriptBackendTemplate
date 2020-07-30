447dc2825372e6ad84545316298c44a51ef4662f
karankurbur <karankurbur@gmail.com>
2020-07-29 21:01:11 -0700

    Mocha Setup

---
+++ test/test.js
@@ -0,0 +1,22 @@
+/* eslint-disable func-names */
+/* eslint-disable node/no-unpublished-require */
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../index");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe("Redeem Endpoints", function () {
+  before(async function () {});
+
+  beforeEach(async function () {});
+
+  it("Succesfully create a redeem.", async function () {
+    const json = {};
+    const res = await chai.request(app).post("/addRedeem").send(json);
+    expect(res.statusCode).to.equals(200);
+  });
+
+  after(async function () {});
+});
